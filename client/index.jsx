90857862d5eac7c020f7ddf02dd197f77745340e
Aimen Altaiyeb <ymnghost@gmail.com>
2019-05-19 05:52:23 -0700

    Initial React Setup

--- client/index.jsx
+++ client/index.jsx
@@ -4,4 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import App from './components/App.jsx'
 
+ReactDOM.render(<App />, document.getElementById('app'));
