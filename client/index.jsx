467c1577a4d219d42670405580dc75da7cac8f65
Aimen Altaiyeb <ymnghost@gmail.com>
2019-05-19 05:47:32 -0700

    Add HTML Mockups from Parent repo

---
+++ client/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
