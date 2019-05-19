82c5fe182bfa041b290d559af9839016a3dd1ff8
Aimen Altaiyeb <ymnghost@gmail.com>
2019-05-19 08:19:37 -0700

    Attempt to bundle bootstrap with webpack

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,1 +1,20 @@
 import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stuff: false
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Testing</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
